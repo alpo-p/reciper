5d334524e7a553b6068620549a2c94ad02c5f616
supea <alpo.panula@gmail.com>
2021-05-10 11:19:54 +0300

    skeleton of HomeScreen

--- src/screens/MainStack/HomeScreen/index.tsx
+++ src/screens/MainStack/HomeScreen/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { View } from 'react-native';
+import SText from '../../../components/SText';
 import STitle from '../../../components/STitle';
+import { styles } from './styles';
 
 const HomeScreen = () => {
+  console.log('Entering home screen');
   return (
-    <View>
+    <View style={styles.homeContainer}>
       <STitle>Toimiii!</STitle>
+      <SText>Täällä ollaan</SText>
     </View>
   );
 };
---
+++ src/screens/MainStack/HomeScreen/styles.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, ViewStyle } from 'react-native';
+import theme from '../../../theme';
+
+interface Styles {
+  homeContainer: ViewStyle,
+}
+
+export const styles: Styles = StyleSheet.create({
+  homeContainer: {
+    height: theme.dimensions.height,
+    marginTop: theme.dimensions.statusBar
+  },
+});
