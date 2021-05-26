03adfb5e1ba6aaf677c6a00291ecc91924e54668
supea <alpo.panula@gmail.com>
2021-05-26 10:43:38 +0300

    Feature: Animated loading icon

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { ActivityIndicator } from 'react-native-paper';
+import theme from '../../theme';
 
 interface Props {
   testID?: string
@@ -8,9 +10,18 @@ interface Props {
 const Loading = (props: Props) => {
   const { testID } = props;
   return (
-    <View testID={testID}>
+    <View style={styles.container} testID={testID}>
+      <ActivityIndicator size='large' color={theme.colors.primaryColor} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    height: theme.dimensions.height + theme.dimensions.statusBar,
+    backgroundColor: 'rgba(0,0,0,.4)',
+    justifyContent: 'center',
+  }
+});
 
 export default Loading;
